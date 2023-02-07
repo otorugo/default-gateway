0b1951074c58744c0cdbd5b5ead080a719d90d71
victorsilva <victorsilva698@gmail.com>
2023-02-06 21:52:02 -0300

    add marshmallow to project

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from src.models.UserModel import db_connector,UserModel
-
-
-db_connector.Base.metadata.create_all(db_connector.engine)
-
-newUser = UserModel(name="Victor Hugo", email="victorsilva698@gmail.com")
-
-session = db_connector.Session()
-session.add(newUser)
-session.commit()
+from src.models.UserModel import UserModel, userSchema, db_connector
+# db_connector.Base.metadata.create_all(db_connector.engine)
+#
+newUser = UserModel(name="Victor Hugo", email="victors@gmail.com")
+#
+# session = db_connector.Session()
+# session.add(newUser)
+# session.commit()
+#
+#
+print(userSchema.dump(newUser))
+#
--- src/models/UserModel.py
+++ src/models/UserModel.py
@@ -1,10 +1,22 @@
+from marshmallow import Schema, fields
 from sqlalchemy import Column, Integer, String, Boolean
+
 from connectors.sql import db_connector
 
 
 class UserModel(db_connector.Base):
     __tablename__ = "User"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(60), nullable=False)
     email = Column(String(60), unique=True, nullable=False)
     deleted = Column(Boolean, default=False, nullable=False)
+
+
+class UserSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+    email = fields.Str()
+    deleted = fields.Boolean()
+
+
+userSchema = UserSchema()
