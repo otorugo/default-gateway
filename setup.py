006ec39baf3c14dae44918b337d33ac5d2c17bfe
victorsilva <victorsilva698@gmail.com>
2023-02-06 21:56:09 -0300

    tests in database creation file

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
 from src.models.UserModel import UserModel, userSchema, db_connector
-# db_connector.Base.metadata.create_all(db_connector.engine)
-#
-newUser = UserModel(name="Victor Hugo", email="victors@gmail.com")
-#
-# session = db_connector.Session()
-# session.add(newUser)
-# session.commit()
-#
-#
-print(userSchema.dump(newUser))
-#
+db_connector.Base.metadata.create_all(db_connector.engine)
+
